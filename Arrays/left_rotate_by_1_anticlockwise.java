d459b72d7d15644fcdaac8a87779396c602fbe8d
shreyjain17 <159146918+shreyjain17@users.noreply.github.com>
2024-08-15 23:27:08 +0530

    Create left_rotate_by_1_anticlockwise.java

---
+++ Arrays/left_rotate_by_1_anticlockwise.java
@@ -0,0 +1,19 @@
+import java.util.* ;
+import java.io.*; 
+
+
+public class Solution {
+
+    static int[] rotateArray(int[] arr, int n) {
+        int temp;
+        temp=arr[0];
+        for(int i=1;i<n;i++){
+            arr[i-1]=arr[i];
+            
+        }
+
+        arr[n-1]=temp;
+        return arr;
+
+    }
+}
